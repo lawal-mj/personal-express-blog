0bec4228cf6d8c65465c424482f02f376e864805
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-07-23 02:44:50 +0100

    add boilerplate code

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+    res.render('index')
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is listening.....")
+})
